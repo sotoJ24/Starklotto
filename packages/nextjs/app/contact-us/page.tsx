59859fbea93fb9664976fe54093d53e73b2af756
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-01-17 01:06:06 -0500

    Contact us form has ben upgrated

--- packages/nextjs/app/contact-us/page.tsx
+++ packages/nextjs/app/contact-us/page.tsx
@@ -1,12 +1,114 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { UserIcon, EnvelopeIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import { ContactUsFormData } from "~~/interfaces/contact-us";
+import { CONTACT_US_SHEET_SCRIPT } from "~~/utils/Constants";
 
 const ContactUsPage = () => {
+
+  const [formData, setFormData] = useState<ContactUsFormData>({
+    name: "",
+    email: "",
+    message: ""
+  });
+  const [errors, setErrors] = useState<ContactUsFormData>({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitError, setSubmitError] = useState("");
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+    // Validar el campo correspondiente y actualizar errores
+    if (name === "name" && value !== "") {
+        setErrors(prev => ({ ...prev, name: "" }));
+    }
+    if (name === "email") {
+        if (value === "") {
+            setErrors(prev => ({ ...prev, email: "Email is required" }));
+        } else if (!/\S+@\S+\.\S+/.test(value)) {
+            setErrors(prev => ({ ...prev, email: "Email is invalid" }));
+        } else {
+            setErrors(prev => ({ ...prev, email: "" }));
+        }
+    }
+    if (name === "message" && value !== "") {
+        setErrors(prev => ({ ...prev, message: "" }));
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    let hasErrors = false;
+
+    if (formData.name === "") {
+      setErrors(prev => ({ ...prev, name: "Name is required" }));
+      hasErrors = true;
+    }
+    if (formData.email === "") {
+      setErrors(prev => ({ ...prev, email: "Email is required" }));
+      hasErrors = true;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      setErrors(prev => ({ ...prev, email: "Email is invalid" }));
+      hasErrors = true;
+    }
+    if (formData.message === "") {
+      setErrors(prev => ({ ...prev, message: "Message is required" }));
+      hasErrors = true;
+    }
+
+    if (hasErrors) return; // Detener si hay errores
+
+    try {
+        await fetch(CONTACT_US_SHEET_SCRIPT, {
+            method: "POST",
+            mode: 'no-cors', 
+            cache: 'no-cache', 
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        });
+
+        setIsSubmitted(true);
+    } catch (error) {
+        setSubmitError("There was a problem sending the message.");
+    }
+
+    // Reiniciar el formulario
+    setFormData({ name: "", email: "", message: "" });
+    setErrors({ name: "", email: "", message: "" });
+    setIsSubmitting(false);
+  };
+
+  useEffect(() => {
+    if(submitError){
+      setTimeout(() => {
+        setSubmitError("");
+      }, 3000);
+    }
+
+    if (isSubmitted) {
+      setTimeout(() => {
+        setIsSubmitted(false);
+      }, 3000);
+    }
+  }, [isSubmitted, submitError]);
+
   return (
     <div className="text-white text-center py-20">
       <h1 className="text-4xl font-bold">Contact Us</h1>
       <p className="mt-4 text-lg">Have any questions? Reach out to us!</p>
-      <form className="mt-8 max-w-md mx-auto bg-[#1A1A1A] p-4 rounded">
+      <form onSubmit={onSubmit} className="mt-8 max-w-md mx-auto bg-[#1A1A1A] p-4 rounded">
         <div className="mb-4">
           <label className="flex items-center text-left mb-2" htmlFor="fullName">
             Full Name
@@ -14,11 +116,13 @@ const ContactUsPage = () => {
           </label>
           <input
             type="text"
-            id="fullName"
+            name="name" 
+            value={formData.name}
+            onChange={handleChange}                                    
             className="w-full p-2 border border-gray-300 rounded bg-[#2A2A2A] text-white text-sm"
             placeholder="YOUR NAME"
-            required
           />
+          {errors.name && <p className="text-red-500">{errors.name}</p>}
         </div>
 
         <div className="mb-4">
@@ -28,11 +132,13 @@ const ContactUsPage = () => {
           </label>
           <input
             type="email"
-            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
             className="w-full p-2 border border-gray-300 rounded bg-[#2A2A2A] text-white text-sm"
             placeholder="YOUR@EMAIL.COM"
-            required
           />
+          {errors.email && <p className="text-red-500">{errors.email}</p>}
         </div>
 
         <div className="mb-4">
@@ -41,18 +147,24 @@ const ContactUsPage = () => {
             <PencilSquareIcon className="h-5 w-5 text-white ml-2" />
           </label>
           <textarea
-            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
             className="w-full p-2 border border-gray-300 rounded bg-[#2A2A2A] text-white text-sm"
             rows={4}
             placeholder="YOUR MESSAGE..."
-            required
           />
+          {errors.message && <p className="text-red-500">{errors.message}</p>}
         </div>
 
-        <button type="submit" className="bg-gradient-to-r from-[#3A0909] to-[#000000] text-white py-2 px-4 rounded">
-          Send Message
+        <button type="submit" className="bg-gradient-to-r from-[#3A0909] to-[#000000] text-white py-2 px-4 rounded" disabled={isSubmitting}>
+          {isSubmitting ? "Sending..." : "Contact Us"}
         </button>
       </form>
+      {submitError && <p className="text-red-500 mt-4">{submitError}</p>}   
+      {
+        isSubmitted && !submitError && <p className="mt-4 text-yellow-400 text-lg">Message sent successfully!</p>
+      }
     </div>
   );
 };
---
+++ packages/nextjs/interfaces/contact-us/index.ts
@@ -0,0 +1,6 @@
+export type ContactUsFormData = {
+  name: string;
+  email: string;
+  message: string;
+}
+
