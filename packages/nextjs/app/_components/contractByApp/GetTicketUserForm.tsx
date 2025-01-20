ffa314d1f2e99e293d97e9d2a524f695606bfc4d
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-01-19 21:10:42 -0500

    Pretier

--- packages/nextjs/app/_components/contractByApp/GetTicketUserForm.tsx
+++ packages/nextjs/app/_components/contractByApp/GetTicketUserForm.tsx
@@ -102,7 +102,9 @@ export const GetTicketUserForm = ({
 
       {data !== null && data !== undefined && (
         <>
-          <h1 className="text-center text-2xl font-bold">Tickets Found: {ticketsId.length}</h1>
+          <h1 className="text-center text-2xl font-bold">
+            Tickets Found: {ticketsId.length}
+          </h1>
           <p className="text-center text-lg font-bold">Search Ticket</p>
           <select
             value={selectedTicketId}
